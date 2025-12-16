090817979932feb5d3ec3cd39f2101458b711671
ouk <ragcarib@naver.com>
2025-12-16 14:50:10 +0900

    오류수정
    Options Flow 오류수정

--- custom_components/citywaste/config_flow.py
+++ custom_components/citywaste/config_flow.py
@@ -68,7 +68,7 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
 
     def __init__(self, config_entry: config_entries.ConfigEntry):
         """Initialize options flow."""
-        self.config_entry = config_entry
+        #self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         """Handle options flow."""
